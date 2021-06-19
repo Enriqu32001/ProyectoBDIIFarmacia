157bb2a8abea2023a5eb27172ae371ccbe568b06
Fredd Avilez <freddenderson@gmail.com>
2021-06-19 06:39:41 +0000

    first commit

---
+++ FarmciaApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FarmciaApp.MvcApplication" Language="C#" %>
---
+++ FarmciaApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FarmciaApp.Startup))]
+namespace FarmciaApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
